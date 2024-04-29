e32d248dcdfd2370ffbad83259d9603b343868ad
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-04-29 16:39:27 -0400

    Update signout.php

--- signout.php
+++ signout.php
@@ -1,12 +1,12 @@
 <?php
 session_start(); /* Starts the session */
-session_destroy(); /* Destroy started session */
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach($cookies as $cookie) {
-        $parts = explode('=', $cookie);
+session_destroy(); /* Destroy started session i.e. all session vars */
+if (isset($_SERVER['HTTP_COOKIE'])) { //if there are cookies
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']); //split cookies into array
+    foreach($cookies as $cookie) { //for each cookie
+        $parts = explode('=', $cookie); 
         $name = trim($parts[0]);
-        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000); //delete it
         setcookie($name, '', time()-1000, '/');
 	}
 }
