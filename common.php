0aa5d6d9a562b51c6a5c64e678c6696524378602
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-04-29 16:08:41 -0400

    Update common.php

--- common.php
+++ common.php
@@ -1,27 +1,26 @@
 <?php
-function encrypt($key) {
+function encrypt($key) { //function to return encrypted string (for password)
 	$ciphering = "AES-128-CTR";
 	$options = 0;
 	$encryption_iv = '1234567891011121';
 	$encryption_key = "MyEncryption";
-	$encryption = openssl_encrypt($key, $ciphering,
-            $encryption_key, $options, $encryption_iv);
+	$encryption = openssl_encrypt($key, $ciphering, $encryption_key, $options, $encryption_iv); //encrypt function
 	return $encryption;
 }
 
-function decrypt($key) {
+function decrypt($key) { //function to decrypt if necessary
 	$ciphering = "AES-128-CTR";
 	$decryption_iv = '1234567891011121';
 	$decryption_key = "MyEncryption";
 	$options = 0;
 	$decryption = openssl_decrypt ($key, $ciphering, $decryption_key, $options, $decryption_iv);
-		return $decryption;
+	return $decryption;
 }
-function banner() {
+function banner() { //print html banner elements
 	echo '<div class = "bannerimg">';
-	if ($_SESSION['username']) {
+	if ($_SESSION['username']) { // (this is for the sign in/logout text) if user is logged in create link to sign out,
 	echo '<div class = "useroptions"><a href = "./signout.php">Sign Out</a></div>';
-	} else {
+	} else { //else print sign in/up
 	echo '<div class = "useroptions"><a href = "./signup.php">Sign Up</a> or <a href = "./login.php">Login</a></div>';
 	}
 	echo '</div>
