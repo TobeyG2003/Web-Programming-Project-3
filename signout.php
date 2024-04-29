7c44aa68cb1d097d37c7337ec9f5b20d0280d007
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-04-28 23:24:32 -0400

    Update signout.php

--- signout.php
+++ signout.php
@@ -2,6 +2,7 @@
 session_start(); /* Starts the session */
 session_destroy(); /* Destroy started session */
 setcookie("username", "", time() - 3600); //delete cookie
+setcookie("role", "", time() - 3600); //delete cookie
 header("location:home.php"); /*return to front page*/
 exit;
 ?>
